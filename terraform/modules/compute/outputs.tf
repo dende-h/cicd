9aec7d2662e6d7abc64d3043877d6e958f14dabd
Hiroya Shitogiden <miyuki_dende@yahoo.co.jp>
2023-10-05 15:31:29 +0900

    fix:terraformOutput修正

--- terraform/modules/compute/outputs.tf
+++ terraform/modules/compute/outputs.tf
@@ -1,4 +1,4 @@
 output "ec2_public_ip" {
   description = "The public IP of the EC2 instance"
-  value       = aws_instance.terraform_ec2.public_ip
+  value       = aws_eip.terraform-eip.public_ip
 }
