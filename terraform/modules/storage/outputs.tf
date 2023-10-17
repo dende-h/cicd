8c78d01df2d51fab2653751fcfbd02579fe6a814
Hiroya Shitogiden <miyuki_dende@yahoo.co.jp>
2023-10-17 09:14:55 +0900

    fix:Outputの引数修正

--- terraform/modules/storage/outputs.tf
+++ terraform/modules/storage/outputs.tf
@@ -1,4 +1,4 @@
 output "s3_bucket_name" {
-  value       = aws_s3_bucket.terraform_s3_bucket.name
+  value       = aws_s3_bucket.terraform_s3_bucket.bucket
   description = "The AmazonS3 bucket name"
 }
