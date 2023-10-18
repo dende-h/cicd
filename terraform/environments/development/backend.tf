52d166c787bb8a42537089c104b0215dc6de19b8
Hiroya Shitogiden <miyuki_dende@yahoo.co.jp>
2023-10-18 14:33:13 +0900

    fix:backend設定変更

--- terraform/environments/development/backend.tf
+++ terraform/environments/development/backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "s3" {
-    #下記を手順4で作成したS3の名前に書き換えてください
-    bucket         = var.tfstate_storage 
-    
     key            = "development/terraform.tfstate"
     region         = "ap-northeast-1"  
     encrypt        = true 
